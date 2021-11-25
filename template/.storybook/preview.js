0420ec98cd834e9f850a16095f53c4df24a3e367
Tanner Lehett <johntannerlehett@gmail.com>
2021-11-24 19:08:53 -0500

    Update storybook config to use webpack5 builder.

--- template/.storybook/preview.js
+++ template/.storybook/preview.js
@@ -1,43 +1,13 @@
-import '@storybook/addon-console';
-
-export const parameters = {
-    controls: {
-        expanded: true,
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
-        },
-    },
-    viewport: {
-        viewports: {
-            matSmall: {
-                name: 'Material Small',
-                styles: {
-                    width: '600px',
-                    height: '600px',
-                },
-            },
-            matMedium: {
-                name: 'Material Medium',
-                styles: {
-                    width: '960px',
-                    height: '960px',
-                },
-            },
-            matLarge: {
-                name: 'Material Large',
-                styles: {
-                    width: '1280px',
-                    height: '600px',
-                },
-            },
-            matExtraLarge: {
-                name: 'Material Extra Large',
-                styles: {
-                    width: '1920px',
-                    height: '1080px',
-                },
-            },
-        },
-    },
-};
+module.exports = {
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+    ],
+    "core": {
+        "builder": "webpack5"
+    }
+}
